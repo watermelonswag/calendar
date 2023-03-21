6df166fedb41fe6cf1c5689bba6202e9e27996da
Jeonghan Kim <gtrew9606@naver.com>
2023-03-22 02:21:41 +0900

    second commit

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -14,77 +14,77 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import MainIcon from '../../images/MainIcon.png';
 import styles from './Signin.module.css';
 
+
 const theme = createTheme();
 
 export default function SignIn() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      id: data.get('id'),
-      password: data.get('password'),
+    id: data.get('id'),
+    password: data.get('password'),
     });
-  };
+};
 
-  return (
+return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="xs">
+
         <CssBaseline />
         <Box
-          sx={{
+        sx={{
             marginTop: 8,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-          }}
+        }}
         >
-          <img className={styles.mainIcon} src={MainIcon} />
-          <div className={styles.container}>
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-              <TextField
-                className={styles.textField}
-                margin="normal"
-                required
-                fullWidth
-                id="id"
-                label="이메일"
-                name="id"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                className={styles.textField}
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="비밀번호"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="아이디 기억하기"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ backgroundColor: '#0040ff', mt: 3, mb: 2 }}
-              >
-                로그인
-              </Button>
-              <Link className={styles.forgotPassword} href="#" variant="body1">
-                비밀번호를 까먹으셨나?
-              </Link>
-            </Box>
-            <Button className={styles.signUp} href="#">
-              계정을 만들어보자!
+
+        <img className={styles.mainIcon} src={MainIcon} />
+
+        <div className={styles.container}>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField className={styles.textField}
+            margin="normal"
+            required
+            fullWidth
+            id="id"
+            label="이메일"
+            name="id"
+            autoComplete="email"
+            autoFocus
+            />
+            <TextField className={styles.textField}
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="비밀번호"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            />
+            <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="아이디 기억하기"
+            />
+            <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ backgroundColor:'#0040ff', mt: 3, mb: 2 }}
+            >
+            로그인
             </Button>
-          </div>
+            <Button className={styles.signUp} href="#">계정을 만들어보자!</Button>
         </Box>
-      </Container>
+        <Link className={styles.forgotPassword} href="#" variant="body1">
+              비밀번호를 까먹으셨나?
+        </Link>
+        </div>
+        </Box>
+    </Container>
     </ThemeProvider>
-  );
+);
 }
